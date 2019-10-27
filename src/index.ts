23d30722dea69b226430582e321c83ca870a32de
lemon-clown <me.clown.lemon@gmail.com>
2019-10-27 21:24:19 +0800

    feat: update entry file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+import { ARRAY_T_TYPE } from './schema/array'
+import { BOOLEAN_T_TYPE } from './schema/boolean'
+import { INTEGER_T_TYPE } from './schema/integer'
+import { NUMBER_T_TYPE } from './schema/number'
+import { STRING_T_TYPE } from './schema/string'
+import { DataSchemaParserMaster } from './parser/_master'
+import { ArrayDataSchemaParser } from './parser/array'
+import { BooleanDataSchemaParser } from './parser/boolean'
+import { IntegerDataSchemaParser } from './parser/integer'
+import { NumberDataSchemaParser } from './parser/number'
+import { StringDataSchemaParser } from './parser/string'
+import { DataValidatorMaster } from './validator/_master'
+import { ArrayDataValidatorFactory } from './validator/array'
+import { BooleanDataValidatorFactory } from './validator/boolean'
+import { IntegerDataValidatorFactory } from './validator/integer'
+import { NumberDataValidatorFactory } from './validator/number'
+import { StringDataValidatorFactory } from './validator/string'
+
+
 // export DataSchemas
 export * from './schema/_base'
 export * from './schema/boolean'
@@ -7,6 +26,7 @@ export * from './schema/string'
 
 // export DataSchemaParsers
 export * from './parser/_base'
+export * from './parser/_master'
 export * from './parser/boolean'
 export * from './parser/integer'
 export * from './parser/number'
@@ -22,3 +42,25 @@ export * from './validator/string'
 // export utils
 export * from './_util/cover-util'
 export * from './_util/type-util'
+
+
+/**
+ * 默认的 ParserMaster；支持类型包括：array, boolean, integer, number, string
+ */
+export const parserMaster = new DataSchemaParserMaster()
+parserMaster.registerParser(ARRAY_T_TYPE, new ArrayDataSchemaParser(parserMaster))
+parserMaster.registerParser(BOOLEAN_T_TYPE, new BooleanDataSchemaParser())
+parserMaster.registerParser(INTEGER_T_TYPE, new IntegerDataSchemaParser())
+parserMaster.registerParser(NUMBER_T_TYPE, new NumberDataSchemaParser())
+parserMaster.registerParser(STRING_T_TYPE, new StringDataSchemaParser())
+
+
+/**
+ * 默认的 DataValidatorMaster；支持类型包括：array, boolean, integer, number, string
+ */
+export const validatorMaster = new DataValidatorMaster()
+validatorMaster.registerValidatorFactory(ARRAY_T_TYPE, new ArrayDataValidatorFactory(validatorMaster))
+validatorMaster.registerValidatorFactory(BOOLEAN_T_TYPE, new BooleanDataValidatorFactory())
+validatorMaster.registerValidatorFactory(INTEGER_T_TYPE, new IntegerDataValidatorFactory())
+validatorMaster.registerValidatorFactory(NUMBER_T_TYPE, new NumberDataValidatorFactory())
+validatorMaster.registerValidatorFactory(STRING_T_TYPE, new StringDataValidatorFactory())
