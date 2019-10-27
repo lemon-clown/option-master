63c0282d57bd9b63f93b8da603e805264be8c672
lemon-clown <me.clown.lemon@gmail.com>
2019-10-27 23:47:27 +0800

    feat: update entry file

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,21 @@ import { ARRAY_T_TYPE } from './schema/array'
 import { BOOLEAN_T_TYPE } from './schema/boolean'
 import { INTEGER_T_TYPE } from './schema/integer'
 import { NUMBER_T_TYPE } from './schema/number'
+import { OBJECT_T_TYPE } from './schema/object'
 import { STRING_T_TYPE } from './schema/string'
 import { DataSchemaParserMaster } from './parser/_master'
 import { ArrayDataSchemaParser } from './parser/array'
 import { BooleanDataSchemaParser } from './parser/boolean'
 import { IntegerDataSchemaParser } from './parser/integer'
 import { NumberDataSchemaParser } from './parser/number'
+import { ObjectDataSchemaParser } from './parser/object'
 import { StringDataSchemaParser } from './parser/string'
 import { DataValidatorMaster } from './validator/_master'
 import { ArrayDataValidatorFactory } from './validator/array'
 import { BooleanDataValidatorFactory } from './validator/boolean'
 import { IntegerDataValidatorFactory } from './validator/integer'
 import { NumberDataValidatorFactory } from './validator/number'
+import { ObjectDataValidatorFactory } from './validator/object'
 import { StringDataValidatorFactory } from './validator/string'
 
 
@@ -22,6 +25,7 @@ export * from './schema/_base'
 export * from './schema/boolean'
 export * from './schema/integer'
 export * from './schema/number'
+export * from './schema/object'
 export * from './schema/string'
 
 // export DataSchemaParsers
@@ -30,6 +34,7 @@ export * from './parser/_master'
 export * from './parser/boolean'
 export * from './parser/integer'
 export * from './parser/number'
+export * from './parser/object'
 export * from './parser/string'
 
 // export DataValidators
@@ -37,6 +42,7 @@ export * from './validator/_base'
 export * from './validator/boolean'
 export * from './validator/integer'
 export * from './validator/number'
+export * from './validator/object'
 export * from './validator/string'
 
 // export utils
@@ -52,6 +58,7 @@ parserMaster.registerParser(ARRAY_T_TYPE, new ArrayDataSchemaParser(parserMaster
 parserMaster.registerParser(BOOLEAN_T_TYPE, new BooleanDataSchemaParser())
 parserMaster.registerParser(INTEGER_T_TYPE, new IntegerDataSchemaParser())
 parserMaster.registerParser(NUMBER_T_TYPE, new NumberDataSchemaParser())
+parserMaster.registerParser(OBJECT_T_TYPE, new ObjectDataSchemaParser(parserMaster))
 parserMaster.registerParser(STRING_T_TYPE, new StringDataSchemaParser())
 
 
@@ -63,4 +70,5 @@ validatorMaster.registerValidatorFactory(ARRAY_T_TYPE, new ArrayDataValidatorFac
 validatorMaster.registerValidatorFactory(BOOLEAN_T_TYPE, new BooleanDataValidatorFactory())
 validatorMaster.registerValidatorFactory(INTEGER_T_TYPE, new IntegerDataValidatorFactory())
 validatorMaster.registerValidatorFactory(NUMBER_T_TYPE, new NumberDataValidatorFactory())
+validatorMaster.registerValidatorFactory(OBJECT_T_TYPE, new ObjectDataValidatorFactory(validatorMaster))
 validatorMaster.registerValidatorFactory(STRING_T_TYPE, new StringDataValidatorFactory())
