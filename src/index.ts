350f9e4730b79c8b33283c0ab3d086cab010b289
lemon-clown <me.clown.lemon@gmail.com>
2019-10-27 14:39:05 +0800

    feat: add entry file

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// export DataSchemas
+export * from './schema/_base'
+export * from './schema/boolean'
+export * from './schema/integer'
+export * from './schema/number'
+export * from './schema/string'
+
+// export DataSchemaParsers
+export * from './parser/_base'
+export * from './parser/boolean'
+export * from './parser/integer'
+export * from './parser/number'
+export * from './parser/string'
+
+// export DataValidators
+export * from './validator/_base'
+export * from './validator/boolean'
+export * from './validator/integer'
+export * from './validator/number'
+export * from './validator/string'
+
+// export utils
+export * from './_util/cover-util'
+export * from './_util/type-util'
